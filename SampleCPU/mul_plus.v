cf7ff6fad68952e218175405e04f3acb3e6b2659
Shepherd23333 <927561263l@gmail.com>
2024-12-27 00:18:56 +0800

    preparation

---
+++ SampleCPU/mul_plus.v
@@ -0,0 +1,13 @@
+`timescale 1ns / 1ps
+module mul_plus(
+    input clk,
+    input start_i,
+    input mul_sign,  
+    input [31:0] opdata1_i,
+    input [31:0] opdata2_i,
+    output [63:0] result_o,
+    output        ready_o
+    );
+
+    
+endmodule
