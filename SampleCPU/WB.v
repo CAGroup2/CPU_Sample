779cc1a7ffa535451686afc4a48ca78171f095d7
Shepherd23333 <927561263l@gmail.com>
2025-01-04 23:13:54 +0800

    multiplier finished

--- SampleCPU/WB.v
+++ SampleCPU/WB.v
@@ -1,24 +1,26 @@
 `include "lib/defines.vh"
 module WB(
+    //时钟信号
     input wire clk,
+    //复位信号
     input wire rst,
-    // input wire flush,
+    //暂停信号
     input wire [`StallBus-1:0] stall,
-
+    //接收MEM段的值
     input wire [`MEM_TO_WB_WD-1:0] mem_to_wb_bus,
-
+    input wire[65:0] mem_to_wb1,
+    //写回reg_array[31:0]的值
     output wire [`WB_TO_RF_WD-1:0] wb_to_rf_bus,
-
+    //发送给ID段的值
     output wire [37:0] wb_to_id_bus,
+    output wire[65:0] wb_to_id_2, 
+    //发送给regfile的值
+    output wire[65:0]wb_to_id_wf,
     
     output wire [31:0] debug_wb_pc,
     output wire [3:0] debug_wb_rf_wen,
     output wire [4:0] debug_wb_rf_wnum,
-    output wire [31:0] debug_wb_rf_wdata,
-    
-    input wire[65:0] mem_to_wb1 ,
-    output wire[65:0]wb_to_id_wf,
-    output wire[65:0] wb_to_id_2 
+    output wire [31:0] debug_wb_rf_wdata
     );
 
     reg [`MEM_TO_WB_WD-1:0] mem_to_wb_bus_r;
@@ -29,14 +31,11 @@ module WB(
             mem_to_wb_bus_r <= `MEM_TO_WB_WD'b0;
             mem_to_wb1_r <= 66'b0;
         end
-        // else if (flush) begin
-        //     mem_to_wb_bus_r <= `MEM_TO_WB_WD'b0;
-        // end
-        else if (stall[4]==`Stop && stall[5]==`NoStop) begin
+        else if (stall[4]==`Stop && stall[5]==`NoStop) begin    //暂停时刷新管道
             mem_to_wb_bus_r <= `MEM_TO_WB_WD'b0;
             mem_to_wb1_r <= 66'b0;
         end
-        else if (stall[4]==`NoStop) begin
+        else if (stall[4]==`NoStop) begin   //接收MEM段的值
             mem_to_wb_bus_r <= mem_to_wb_bus;
             mem_to_wb1_r <= mem_to_wb1;
         end
@@ -46,7 +45,7 @@ module WB(
     wire rf_we;
     wire [4:0] rf_waddr;
     wire [31:0] rf_wdata;
-
+    //hi,lo寄存器的使能信号与值
     wire w_hi_we;
     wire w_lo_we;
     wire [31:0]hi_i;
@@ -65,24 +64,26 @@ module WB(
         w_lo_we,
         hi_i,
         lo_i
-    } = mem_to_wb1_r;
-    
+    } = mem_to_wb1_r;   //接收MEM段的值
+    //包括了写hi,lo寄存器的使能信号，用于判断是否进行写寄存器的操作
+    //以及将要写入hi,lo寄存器的值
+    //如果不写，则此处为0，且使能信号为0
     assign wb_to_id_wf=
     {
         w_hi_we,
         w_lo_we,
         hi_i,
         lo_i
-    };
-    
+    };  //发送到regfile.v，内容同上
     assign wb_to_id_2=
     {
         w_hi_we,
         w_lo_we,
         hi_i,
         lo_i
-    };
-    
+    };  //发送到ID段
+    //WB段要发送给ID段中的regfile.v
+    //用于解决下一条指令要用到上一条指令存入hi,lo寄存器值的问题
     assign wb_to_rf_bus = {
         rf_we,
         rf_waddr,
@@ -92,8 +93,9 @@ module WB(
         rf_we,
         rf_waddr,
         rf_wdata
-    };
-
+    };  //发送到ID段
+    //若当前指令需要取前面还未存入寄存器的值，则将指令由WB段提前发给ID段，
+    //再由ID段发送到regfile.v文件中，进行赋给rs和rt所需要的寄存器的值
     assign debug_wb_pc = wb_pc;
     assign debug_wb_rf_wen = {4{rf_we}};
     assign debug_wb_rf_wnum = rf_waddr;
--- SampleCPU/mul_plus.v
+++ SampleCPU/mul_plus.v
@@ -1,13 +1,86 @@
 `timescale 1ns / 1ps
 module mul_plus(
+    //时钟信号
     input clk,
+    //开始标志
     input start_i,
-    input mul_sign,  
+    //是否为有符号乘法
+    input mul_sign,
+    //操作数1
     input [31:0] opdata1_i,
+    //操作数2
     input [31:0] opdata2_i,
+    //结果
     output [63:0] result_o,
+    //结束标志
     output        ready_o
     );
+    //状态标志，计算时为1
+    reg judge;
+    //被乘数
+    reg  [63:0] multiplicand;
+    //乘数
+    reg [31:0] multiplier;
+    //存储被乘数的临时变量
+    wire [63:0] temporary_value;
+    //临时结果
+    reg [63:0] mul_temporary;
+    //结果符号
+    reg result_sign;
+    //更新状态
+    always @(posedge clk) begin
+        if (!start_i || ready_o) begin
+            judge <= 1'b0;
+        end
+        else begin
+            judge <= 1'b1;
+        end
+    end
 
+    wire op1_sign;
+    wire op2_sign;
+    wire [31:0] op1_absolute;
+    wire [31:0] op2_absolute;
+    //判断两个操作数的符号，并获取其绝对值
+    assign op1_sign = mul_sign & opdata1_i[31];
+    assign op2_sign = mul_sign & opdata2_i[31];
+    assign op1_absolute = op1_sign ? (~opdata1_i+1) : opdata1_i;
+    assign op2_absolute = op2_sign ? (~opdata2_i+1) : opdata2_i;
+
+    always @ (posedge clk) begin //处理被乘数
+        if (judge) begin
+            multiplicand <= {multiplicand[62:0],1'b0};  //左移1位
+        end
+        else if (start_i) begin
+            multiplicand <= {32'd0,op1_absolute};
+        end
+    end
     
+    always @ (posedge clk) begin //处理乘数
+        if(judge) begin
+            multiplier <= {1'b0,multiplier[31:1]};  //右移1位
+        end
+        else if(start_i) begin
+            multiplier <= op2_absolute;
+        end
+    end
+    //若当前时钟周期的乘数末位为1，则将当前时钟周期的被乘数加到临时结果上
+    assign temporary_value = multiplier[0] ? multiplicand : 64'd0;
+    always @ (posedge clk) begin
+        if (judge) begin
+            mul_temporary <= mul_temporary + temporary_value;
+        end      
+        else if (start_i) begin
+            mul_temporary <= 64'd0;
+        end
+     end
+     
+    always @ (posedge clk) begin    //获取结果符号
+        if (judge) begin
+              result_sign <= op1_sign ^ op2_sign;
+        end
+    end 
+    //计算结果
+    assign result_o = result_sign ? (~mul_temporary+1) : mul_temporary;
+    assign ready_o  = judge & multiplier == 32'b0;
 endmodule
